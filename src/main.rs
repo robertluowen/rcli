02ac92f0a0f1217b1f326c35aed8977e70a36642
robert <757120778@qq.com>
2024-07-23 01:55:17 +0800

    clap test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
-// rcli csv =i input.csv -o output.json -c 1,2,3
+// rcli csv =i input.csv -o output.json -header -d ','
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[command(name ="rcli",version, author,about,long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV ,or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long,value_parser = verify_input_file)]
+    input: String,
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if std::path::Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("Error creating")
+    }
+}
 
 fn main() {
-    println!("Hello, world1!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
