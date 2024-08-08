cbf68898bc5cb53711a09257d4e55a7cd12e00cd
robert <757120778@qq.com>
2024-08-09 02:10:42 +0800

    format

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,13 @@
-// rcli csv =i input.csv -o output.json -header -d ','
+use anyhow::Result;
 use clap::Parser;
-// use csv::Reader;
-// use serde::{Deserialize, Serialize};
-use std::path::Path;
-#[derive(Debug, Parser)]
-#[command(name ="rcli",version, author,about,long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
+use rcli::{process_csv, Opts, SubCommand};
 
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show23 CSV ,or Convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long,value_parser = verify_input_file)]
-    input: String,
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-
-fn verify_input_file(filename: &str) -> Result<String, &'static str> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("Error creating")
-    }
-}
-
-fn main() {
+fn main() -> Result<()> {
     let opts = Opts::parse();
-    println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+            Ok(())
+        }
+    }
 }
---
+++ src/process.rs
@@ -0,0 +1,27 @@
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::fs;
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Player {
+    pub name: String,
+    pub position: String,
+    #[serde(rename = "DOB")]
+    pub dob: String,
+    pub nationality: String,
+    #[serde(rename = "Kit Number")]
+    pub kit: u8,
+}
+
+pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret = Vec::with_capacity(128);
+    for result in reader.deserialize() {
+        let record: Player = result?;
+        ret.push(record);
+    }
+
+    let json = serde_json::to_string(&ret)?;
+    fs::write(output, json)?;
+    Ok(())
+}
