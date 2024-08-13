849a641a56a1eda83aa75c04308f98630b95f1b6
luowenjun <luowenjun@inman.cc>
2024-08-13 19:57:14 +0800

    csv

--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,7 @@
+// use clap::error::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
-use std::fs;
+// use std::fs;
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Player {
@@ -13,15 +14,21 @@ pub struct Player {
     pub kit: u8,
 }
 
+#[allow(unused_variables)]
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    for result in reader.records() {
+      let record = result?;
+      println!("{:?}", record);
+      ret.push(record);
     }
+    // for result in reader.deserialize() {
+    //     let record: Player = result?;
+    //     ret.push(record);
+    // }
 
-    let json = serde_json::to_string(&ret)?;
-    fs::write(output, json)?;
+    // let json = serde_json::to_string(&ret)?;
+    // fs::write(output, json)?;
     Ok(())
 }
