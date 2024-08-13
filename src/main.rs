fdac8d725fd604715dec08ab625fcf99ebfa926f
robert <757120778@qq.com>
2024-08-14 02:01:01 +0800

    add format

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ fn main() -> Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
         SubCommand::Csv(opts) => {
-            process_csv(&opts.input, &opts.output)?;
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                // "output.csv".into()
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
             Ok(())
         }
     }
--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,9 @@
-// use clap::error::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
-// use std::fs;
+use serde_json::Value;
+use std::fs;
+
+use crate::opts::OutputFormat;
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Player {
@@ -15,20 +17,25 @@ pub struct Player {
 }
 
 #[allow(unused_variables)]
-pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
     for result in reader.records() {
-      let record = result?;
-      println!("{:?}", record);
-      ret.push(record);
+        let record = result?;
+        // headers.iter() -> 使用headers的迭代器
+        // record.iter() -> 使用record的迭代器
+        // zip() -> 将使用迭代器合并为一个元祖的迭代器 [(header, record), ...]
+        // collect::<Value>() -> 将迭代器转换为json_value
+        // 函数式编程
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
-    // for result in reader.deserialize() {
-    //     let record: Player = result?;
-    //     ret.push(record);
-    // }
 
-    // let json = serde_json::to_string(&ret)?;
-    // fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+    fs::write(output, content)?;
     Ok(())
 }
